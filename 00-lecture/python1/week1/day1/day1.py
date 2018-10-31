3a930f4246412cf1259512ade1447a69e9b22f74
Weston Harper <wes@tao.team>
2018-10-30 19:10:06 -0600

    added day1

---
+++ 00-lecture/python1/week1/day1/day1.py
@@ -0,0 +1,55 @@
+# 1. Remember things
+  # variables
+  # functions
+  # databases
+# 2. Math/Logic/Conditionals
+  # arithmetic + - / *
+  # if elif else
+  # comparisons == is not <= >= > <
+# 3. Loops
+  # while
+  # for
+# 4. Bonus
+  # print()
+  # Comments
+
+my_float = 1.2
+my_int = 10
+my_bool = False
+None
+my_string = "10"
+my_dict = {
+  "key": "value",
+  "other": 1
+}
+my_tuple = (1, 2, 3, 4)
+# print(my_tuple[0])
+
+# print(type(my_float))
+# print(isinstance(my_float, float))
+
+def my_func():
+  print("hello")
+
+# my_func()
+
+# if 2 == "2":
+#   print("yep")
+# else:
+#   print('nope')
+
+# for(var i = 0; i < 100; i++){}
+
+my_list = [4, 3, 2, 1]
+
+# for value in my_list:
+#   print(value)
+
+# for i in range(0, len(my_list), 1):
+#   print(i)
+
+i = 0
+while i < 100:
+  print(i)
+  # i = i + 1
+  i += 1
